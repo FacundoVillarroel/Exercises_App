390c61cbaea72e38323041a12d43a8326e55f754
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-04-28 19:36:14 -0400

    [typo] - fixed a typo in word awesome

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -36,20 +36,25 @@ const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography fontWeight="700" sx={{ fontSize:{ lg: "44px", xs:"30px"}}} mb= "50px" textAlign="center">
-        Awesom Exercises You <br/> Should Know
+      <Typography
+        fontWeight="700"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
-        <TextField 
+        <TextField
           sx={{
-            input:{
-              fontWeight:"700", 
-              border:"none", 
-              borderRadius:"4px"
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "4px",
             },
-            width: {lg:"800px", xs:"350px"},
-            backgroundColor:"#fff",
-            borderRadius:"40px"
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
           }}
           heigth="76px"
           value={search}
@@ -57,27 +62,33 @@ const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
           placeholder="Search Exercises"
           type="text"
         />
-        <Button className='search-btn'
+        <Button
+          className="search-btn"
           sx={{
-            bgcolor:"#FF2625",
-            color:"#FFF",
-            textTransform:"none",
-            width:{lg:"175px", xs:"80px"},
-            fontSize:{lg:"20px", xs:"14px"},
-            height:"56px",
-            position:"absolute",
-            right:"0"
+            bgcolor: "#FF2625",
+            color: "#FFF",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
           }}
           onClick={handleSearch}
         >
           Search
         </Button>
       </Box>
-      <Box sx={{ position: "relative", width:" 100%", p:"20px"}}>
-          <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} isBodyParts/>
+      <Box sx={{ position: "relative", width: " 100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+          isBodyParts
+        />
       </Box>
     </Stack>
-  )
+  );
 }
 
 export default SearchExercises
